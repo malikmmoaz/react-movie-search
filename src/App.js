8f8c10ab6923fb3b15e75ccd90b6bfbb23cf024d
Malik Muhammad Moaz <40146994+TheHotatoPotato@users.noreply.github.com>
2023-07-06 12:32:06 +0500

    simple search functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
 
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
   const searchMovies = async (searchValue) => {
     const response = await fetch(`${API_URL}&s=${searchValue}`);
     const data = await response.json();
+
+    setMovies(data.Search);
   };
 
-  //  console.log(searchMovies("batman"));
-  console.log(API_URL);
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="flex flex-col p-20 items-center align-center gap-12">
+      <h1>NOT IMDB</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Batman Begins..."
+          value={search}
+          onChange={handleChange}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(search)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="flex flex-row justify-evenly items-center flex-wrap">
+          {movies.map((movie) => (
+            <MovieCard movies={movie} />
+          ))}
+        </div>
+      ) : (
+        <h1>no movies found</h1>
+      )}
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-function MovieCard() {
-  return <div>MovieCard</div>;
+function MovieCard({ movies }) {
+  return (
+    <div className="card w-96 m-6 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={
+            movies?.Poster !== "N/A"
+              ? movies?.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={movies?.Title}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {movies?.Title} ({movies?.Year})
+        </h2>
+        <p></p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">{movies?.Type}</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
